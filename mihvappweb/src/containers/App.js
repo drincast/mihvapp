371bf6faafbae273bf69793b5782d2cf6521788d
drincastX <drincast@gmail.com>
2018-10-02 17:30:56 -0500

    modificaciones

--- mihvappweb/src/containers/App.js
+++ mihvappweb/src/containers/App.js
@@ -2,49 +2,96 @@ import React, { Component } from 'react';
 
 import DataService from '../services/DataService';
 import Header from './header';
+
+import firebase from '../utils/firebase';
+import configApp from '../configapp.json';
 import logo from '../logo.svg';
 import './App.css';
 
 let objDataService = new DataService();
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      dataCV: undefined
-    };
-  }
+class App extends Component {  
+    constructor(props){
+        super(props);
+        this.state = {
+            dataCV: undefined,
+            imgProfile: undefined
+        };
+    } 
 
-  componentDidMount() {
-    objDataService.getDataCV()
-    .then((response) => {
-      this.setState({
-        dataCV: response.data
-      });
-    })
-    .catch( err => console.log(err));
-  }
+    componentWillMount() {
+        objDataService.getDataCV()
+        .then((response) => {
+            this.setState({
+                dataCV: response.data
+            });
+            
+            return firebase.storageRef.child(`imgProfile/${configApp.defIdPerson}/${this.state.dataCV.imgProfile.url}`).getDownloadURL();            
+        })
+        .then(url => {
+            this.setState({
+                imgProfile: url                        
+            });
+        })        
+        .catch( err => console.log(err));
+    }
 
-  //JSON.stringify(objDataService.getDataCV())
+    //JSON.stringify(objDataService.getDataCV())
 
-  test = () => {
-    alert("hola");
-  }
+    test = () => {
+        alert("hola");
+    }
 
-  render() {
+    getNamePerson() {
+        return this.state.dataCV !== undefined ? this.state.dataCV.firtsName : "user";
+    }
 
-    return (
-      <div className="App">
-          <Header logo={logo}></Header>          
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>{this.state.dataCV !== undefined ? this.state.dataCV.firtsName: false}</div>
-        <div>{}</div>
-        <div onClick={this.test}>hola</div>
-      </div>
-    );
-  }
+    getUrlImgProfile() {
+        let res = configApp.defUrlImgProfile;
+
+        if (this.state.dataCV !== undefined){
+            if(this.state.dataCV.imgProfile !== undefined){
+                if(this.state.dataCV.imgProfile.isFirebase){
+                    
+                    if(this.state.dataCV.imgProfile.url !== undefined && this.state.dataCV.imgProfile.url !== null){
+                        //console.log(firebase.storageRef.child);
+                        //console.log(this.state.dataCV);
+                        firebase.storageRef.child(`imgProfile/${configApp.defIdPerson}/${this.state.dataCV.imgProfile.url}`).getDownloadURL()
+                        .then( (url) => {
+                            console.log(url);
+                            return url;
+                        })
+                        .catch(err => console.log(err));
+                    }
+                }
+                else{
+                    if(this.state.dataCV.imgProfile.url !== undefined && this.state.dataCV.imgProfile.url !== null){
+                        res = this.state.dataCV.imgProfile.url;
+                    }
+                }
+                //res = this.state.imgProfile;
+            }
+        }
+
+        console.log('getUrlImgProfile', res);
+        //return res;
+    }
+
+
+    render() {
+        const _urlImgProfile = this.getUrlImgProfile();
+        return (
+            <div className="App">
+                <Header logo={logo} altImg={this.getNamePerson()} urlImg={this.state.imgProfile}></Header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <div>{this.state.dataCV !== undefined ? this.state.dataCV.firtsName: false}</div>
+                <div>{}</div>
+                <div onClick={this.test}>hola</div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- mihvappweb/src/containers/header.js
+++ mihvappweb/src/containers/header.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 const Header = props => {
-    const { logo } = props
+    const { logo, altImg } = props;
+
+    const _getSrcImg = () => {        
+        console.log(props);
+    }
 
     return (
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="divHeaderImgProfile" onClick={_getSrcImg}>
+                <img className="imgHeaderImgProfile" 
+                    alt={props.altImg}
+                    src={props.urlImg}>
+                </img>
+            </div>
+            <h1 className="App-title">Welcome to React</h1>
         </header>
     );
 }
